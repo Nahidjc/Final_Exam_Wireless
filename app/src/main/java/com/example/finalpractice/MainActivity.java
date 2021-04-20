1c627ccac4627a2a71e8340a595d925275bc8662
Nahid2194 <nahidjc80@gmail.com>
2021-04-20 15:09:29 +0600

    fragment add

--- app/src/main/java/com/example/finalpractice/MainActivity.java
+++ app/src/main/java/com/example/finalpractice/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.finalpractice;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void FragmentChangeFunction(View view) {
+        Fragment myFragment = null;
+        if(view.getId()==R.id.BmiId){
+            myFragment = new BmiFragment();
+        }else if(view.getId()==R.id.FarenhiteID){
+            myFragment = new FarenheitFragment();
+        }else if (view.getId()==R.id.CelsiusID){
+            myFragment = new CelsiusFragment();
+        }
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.fragmentID,myFragment);
+        ft.commit();
+
     }
 }
